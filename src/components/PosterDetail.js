94461eb00e2bdbce11780afa5a9d6d9749d77447
loliiiiipop <nun8866@gmail.com>
2017-11-04 02:48:53 -0400

    with image

--- src/components/PosterDetail.js
+++ src/components/PosterDetail.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -8,20 +8,55 @@ const PosterDetail = (props) =>{
   return (
     <Card>
       <CardSection>
-        <Text> {props.list.name} </Text>
+          <Text> {props.list.name} </Text>
       </CardSection>
       <CardSection>
           <Text> {props.list.body} </Text>
       </CardSection>
       <CardSection>
-        <Text> Stars </Text>
+        <View style={styles.headerContentStyle}>
+          <Image style={styles.imageStyle}
+          source={require('./../image/star_empty.png')}
+          />
+          <Image style={styles.imageStyle}
+          source={require('./../image/star_empty.png')}
+          />
+          <Image style={styles.imageStyle}
+          source={require('./../image/star_empty.png')}
+          />
+          <Image style={styles.imageStyle}
+          source={require('./../image/star_empty.png')}
+          />
+          <Image style={styles.imageStyle}
+          source={require('./../image/star_empty.png')}
+          />
+
+        </View>
       </CardSection>
       <CardSection>
         <Text> Hide Comments </Text>
-        <Text> Image </Text>
+        <Image style={styles.imageStyle}
+        source={require('./../image/download.png')}
+        />
       </CardSection>
     </Card>
   );
 };
+
+const styles = {
+  headerContentStyle:{
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'flex-end',
+
+  },
+   header:{
+     backgroundColor: '#D3D3D3',
+   },
+   imageStyle:{
+     height:15,
+     width:15,
+   },
+}
 
 export default PosterDetail;
